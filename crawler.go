5a96f6cb1d78275bff34cea7401df4ae9db072e4
iguchi-masato <iguchi-masa@dmm.com>
2018-06-19 20:01:04 +0900

    クローラーの分離

---
+++ crawler.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const (
+	sbiURL = "https://www.sbisec.co.jp/ETGate"
+)
+
+const (
+	companyName = iota
+	price
+)
+
+// Crawler gets stock info from sbi
+type Crawler struct {
+	Code string
+	// CompanyName string
+}
+
+// ScrapePrice gets stock price from sbi
+func (crw Crawler) ScrapePrice() (string, error) {
+	if len(crw.Code) == 0 {
+		return "", errors.New("Must set stock code")
+	}
+
+	resp, err := http.PostForm(sbiURL, newParams(crw.Code))
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	if err != nil {
+		return "", errors.New("Can not crawl to sbi")
+	}
+
+	doc, _ := goquery.NewDocumentFromReader(resp.Body)
+
+	var priceVal string
+	doc.Find("span.fxx01").Each(func(i int, s *goquery.Selection) {
+		switch i {
+		case companyName:
+			// nop
+		case price:
+			priceVal = strings.Replace(s.Text(), ",", "", -1)
+		}
+	})
+
+	if len(priceVal) == 0 {
+		return "", errors.New("Can not scrape price")
+	}
+	
+	return priceVal, nil
+}
+
+func newParams(code string) url.Values {
+	values := url.Values{}
+	values.Set("_ControlID", "WPLETsiR001Control")
+	values.Set("_PageID", "WPLETsiR001Iser10")
+	values.Set("_DataStoreID", "DSWPLETsiR001Control")
+	values.Set("_ActionID", "clickToSearchStockPriceJP")
+	values.Set("i_dom_flg", "1")
+	values.Set("ref_from", "1")
+	values.Set("ref_to", "20")
+	values.Set("wstm4130_sort_id", "++")
+	values.Set("wstm4130_sort_kbn", "+")
+	values.Set("i_exchange_code", "JPN")
+	values.Set("i_stock_sec", code)
+	values.Set("json_status", "1")
+	values.Set("json_content", "")
+	values.Set("i_output_type", "0")
+	values.Set("qr_keyword", "1")
+	values.Set("qr_suggest", "1")
+	values.Set("qr_sort", "1")
+
+	return values
+}
--- main.go
+++ main.go
@@ -1,71 +1,25 @@
 package main
 
 import (
-	"net/http"
-	"net/url"
-	"strings"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/PuerkitoBio/goquery"
-)
-
-const (
-	sbiURL = "https://www.sbisec.co.jp/ETGate"
-)
-
-const (
-	companyName = iota
-	price
 )
 
 // Handler is executed by AWS Lambda in the main function. Once the request
 // is processed, it returns an Amazon API Gateway response object to AWS Lambda
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	
-	values := url.Values{}
-	values.Set("_ControlID", "WPLETsiR001Control")
-	values.Set("_PageID", "WPLETsiR001Iser10")
-	values.Set("_DataStoreID", "DSWPLETsiR001Control")
-	values.Set("_ActionID", "clickToSearchStockPriceJP")
-	values.Set("i_dom_flg", "1")
-	values.Set("ref_from", "1")
-	values.Set("ref_to", "20")
-	values.Set("wstm4130_sort_id", "++")
-	values.Set("wstm4130_sort_kbn", "+")
-	values.Set("i_exchange_code", "JPN")
-	values.Set("i_stock_sec", "2685")
-	values.Set("json_status", "1")
-	values.Set("json_content", "")
-	values.Set("i_output_type", "0")
-	values.Set("qr_keyword", "1")
-	values.Set("qr_suggest", "1")
-	values.Set("qr_sort", "1")
 
-	resp, err := http.PostForm(sbiURL, values)
+	crw := Crawler{Code: "2654"}
+
+	price, err := crw.ScrapePrice()
 
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
 	}
-	
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	if err != nil {
-	    return events.APIGatewayProxyResponse{}, err
-	}
-
-	var stockPrice string
-	doc.Find("span.fxx01").Each(func(i int, s *goquery.Selection) {
-		switch i {
-			case companyName:
-				// nop
-			case price: 
-				stockPrice = strings.Replace(s.Text(), ",", "", -1)
-		}
-	})
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       stockPrice,
+		Body:       price,
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
